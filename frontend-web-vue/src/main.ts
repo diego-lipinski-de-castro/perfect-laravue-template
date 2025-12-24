d4d259cc1e54f56221046c18dd15319239e2e773
Diego Lipinski <diegocastroh20@gmail.com>
2025-12-24 13:40:18 -0300

    Updated prettier, added mess-detector, added vueuse

--- frontend-web-vue/src/main.ts
+++ frontend-web-vue/src/main.ts
@@ -2,6 +2,7 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { createHead } from '@unhead/vue/client'
 
 import App from './App.vue'
 import router from './router'
@@ -9,6 +10,21 @@ import router from './router'
 const app = createApp(App)
 
 app.use(createPinia())
+
+app.use(
+  createHead({
+    init: [
+      {
+        title: 'Vite App',
+        titleTemplate: '%s | Vite App',
+        htmlAttrs: {
+          lang: 'en',
+        },
+      },
+    ],
+  }),
+)
+
 app.use(router)
 
 app.mount('#app')
